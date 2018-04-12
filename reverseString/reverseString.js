2ede026b0ad642708e3631370a4cceed69766548
banCoder <prevcurban@gmail.com>
2018-04-12 15:29:08 +0200

    Solved reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-var reverseString = function() {
+var reverseString = function(input) { 
+    // recursion
+    //if (input == "") {
+    //    return "";
+    //}
+    //else return reverseString(input.substr(1)) + input.charAt(0);
 
+    return input.split("").reverse().join("");
 }
 
 module.exports = reverseString
