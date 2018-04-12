af4127cd6a125f87a623a508b61bdb5658c550dd
banCoder <prevcurban@gmail.com>
2018-04-12 15:51:06 +0200

    Solved removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-var removeFromArray = function() {
+var removeFromArray = function(collection) {
+    // captures elements to remove from arguments passed in
+    let args = Array.from(arguments).slice(1);
 
+    // remove each element if it exists
+    for (let i = 0, j = args.length; i < j; i++) {
+        let index = collection.indexOf(args[i]);
+        if (index > -1) collection.splice(index, 1);
+    }
+    return collection;
 }
 
 module.exports = removeFromArray
