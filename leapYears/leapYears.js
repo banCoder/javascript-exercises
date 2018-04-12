dced6947bd3502a5674e4859b37eb69dca6e17dd
banCoder <prevcurban@gmail.com>
2018-04-12 16:43:52 +0200

    Solved leapYears

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,5 @@
 var leapYears = function() {
-
+    return ((arguments[0] % 100 == 0 && arguments[0] % 400 == 0) || (arguments[0] % 100 != 0 && arguments[0] % 4 == 0));
 }
 
 module.exports = leapYears
