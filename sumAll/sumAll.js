85fb40090d195b17deded7a578fe0c3e85f44f38
banCoder <prevcurban@gmail.com>
2018-04-12 16:24:54 +0200

    Solved sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,20 @@
 var sumAll = function() {
+    // check parameters
+    if (typeof arguments[0] != "number" || typeof arguments[1] != "number" || arguments[0] < 0 || arguments[1] < 0) {
+        return "ERROR";
+    }
 
+    let sum = 0;    
+    if (arguments[0] < arguments[1]) {
+        for (let i = arguments[0], j = arguments[1]; i <= j; i++) {
+            sum += i;
+        }
+    } else {
+        for (let j = arguments[0], i = arguments[1]; i <= j; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 }
 
 module.exports = sumAll
